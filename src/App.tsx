d7f90fa968aa8bcc526142e72e0dbf3964706caa
rishtansoft <rishtansfot@gmail.com>
2024-09-20 15:15:54 +0500

    react and typescript install

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+// 4. Trello clone
+
+// 1. Ahmadjon
+// 2. Nurmuhammad Eshonov
+// 3. Abdulloh
+
+// 1. Okifxon
+// 2. Dilmurodjon
+// 3. Ahrorbek
+
+// 1. Lochinbek
+// 2. Nurmuhammad Olimov
+// 3. Sherzdbek
+
+// 1. Jahongir
+// 2. Abdurahmon
+// 3. Shavkatbek
+// 4. Ibrohimjon
+
+
+
+function App() {
+  return (
+    <div>App</div>
+  )
+}
+
+export default App
